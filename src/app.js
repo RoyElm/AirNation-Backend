305163dea6f00a9dabad2fd62176891278c3ca6b
RoyElm <roye456@gmail.com>
2021-06-28 12:33:18 +0300

    mongodb

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
-global.config = require(process.env.NODE_ENV === "production" ? "./env/config-prod.json" : "./env/config-dev.json");
+global.config = require(process.env.NODE_ENV === "production" ? "../env/config-prod.json" : "../env/config-dev.json");
 const express = require("express");
-const authController = require("./controller-layer/auth-controller");
-const adminController = require("./controller-layer/admin-controller");
-const flightController = require("./controller-layer/flight-controller");
-const articleController = require("./controller-layer/article-controller");
-const orderFlightController = require("./controller-layer/order-flight-controller");
+const authController = require("../controller-layer/auth-controller");
+const adminController = require("../controller-layer/admin-controller");
+const flightController = require("../controller-layer/flight-controller");
+const articleController = require("../controller-layer/article-controller");
+const orderFlightController = require("../controller-layer/order-flight-controller");
 const cors = require("cors");
 const fileUpload = require("express-fileupload");
 const server = express();
+const serverless = require("serverless-http");
 
 // Enable sending an receiving cookies from the front:
 
@@ -23,6 +24,8 @@ server.use("/api/article", articleController);
 server.use("/api/flight", flightController);
 server.use("/api/order-flight", orderFlightController);
 
-//listening to environment Port on production or 3001 on developing;
+// //listening to environment Port on production or 3001 on developing;
 const port = process.env.PORT || 3001;
 server.listen(port, () => console.log("Listening...."));
+
+module.exports.handler = serverless(server);
